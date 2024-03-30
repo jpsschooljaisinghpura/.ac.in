8b272a7b0157c756802bfc9287796ee0eec08e4e
JPS School Jaisinghpura <164352297+jpsschooljaisinghpura@users.noreply.github.com>
2024-03-30 11:54:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,232 @@
+// Script File
+
+// Home Section Starts
+var menuNavbar = document.querySelector('.main-navbar')
+var menuBtn = document.querySelector('.main-navbar .menu-btn');
+var menuList = document.querySelector('.main-navbar .nav-list');
+var menuListItems = document.querySelectorAll('.nav-list li a');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menuList.classList.toggle('active');
+});
+
+for(var i = 0; i < menuListItems.length; i++){
+	menuListItems[i].addEventListener('click', menuItemClicked);
+}
+function menuItemClicked(){
+	menuBtn.classList.remove('active');
+	menuList.classList.remove('active');
+}
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 230){
+		homeSection.classList.add('active');
+        
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+// Home Section Ends
+
+
+
+
+// image slider starts
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+
+//team
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 300 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 1500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 1500);
+}
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+
+
+
+
+
+
+let appSlider = document.querySelector('.app-slider .last');
+let appItems = document.querySelectorAll('.app-slider .last .itam');
+let appnext = document.getElementById('appnext');
+let appprev = document.getElementById('appprev');
+let dots1 = document.querySelectorAll('.app-slider .dots1 li');
+
+let lengthItems1 = appItems.length - 1;
+let active1 = 0;
+appnext.onclick = function(){
+    active1 = active1 + 1 <= lengthItems1 ? active1 + 1 : 0;
+    reloadappSlider();
+}
+appprev.onclick = function(){
+    active1 = active1 - 1 >= 0 ? active1 - 1 : lengthItems1;
+    reloadappSlider();
+}
+let refreshInterval1 = setInterval(()=> {appnext.click()}, 3000);
+function reloadappSlider(){
+    appSlider.style.left = -appItems[active1].offsetLeft + 'px';
+    // 
+    let last_active1_dot = document.querySelector('.app-slider .dots1 li.active1');
+    last_active1_dot.classList.remove('active1');
+    dots1[active1].classList.add('active1');
+
+    clearInterval(refreshInterval1);
+    refreshInterval1 = setInterval(()=> {appnext.click()}, 3000);
+
+    
+}
+
+dots1.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active1 = key;
+         reloadappSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadappSlider();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
